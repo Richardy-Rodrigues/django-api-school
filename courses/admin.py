f40a689ea2a35f8e9d6708b1ac50dc0d7b282fd9
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-06-23 13:16:39 -0300

    feat: Created the first models and added the app to the settings

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created', 'updated', 'active')
+
+
+@admin.register(models.Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('course', 'name', 'email', 'evaluation', 'created', 'updated', 'active')
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+# Abstract Model
+class Base(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+# Course Model
+class Course(Base):
+    title = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name  = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.title
+
+# Evaluation Model    
+class Evaluation(Base):
+    course = models.ForeignKey(Course, related_name='evaluations', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField(blank=True, default='')
+    evaluation = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name = 'Evaluation'
+        verbose_name_plural = 'Evaluations'
+        unique_together = ['email', 'course'] # Does not allow an email linked to more than one course
+    
+    def __str__(self):
+        return f"{self.name} rated the course {self.course} with a score {self.evaluation}"
