dd7bd3de4c376d7683b004741c0bf1faff9c0a0b
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2025-06-25 10:47:41 -0300

    feat: throttle configured for the API

--- courses/urls.py
+++ courses/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from . import views
-
+"""
+Version 2
+"""
 router = SimpleRouter()
 router.register('courses', views.CourseViewSet)
 router.register('evaluations', views.EvaluationViewSet)
 
+"""
+Version 1
+"""
 urlpatterns = [
     path('courses/', views.CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>/', views.CourseAPIView.as_view(), name='detail_course'),
